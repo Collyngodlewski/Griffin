408a5a24fc171dde0cf2fceaca8e1f77522049a9
Collyngodlewski <collyngodlewski@gmail.com>
2023-03-05 20:44:58 -0500

    fixed home page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout"
+
+const Index = () => {
+    return(
+        <Layout>
+            <h1>Please Sign In</h1>
+        </Layout>
+    )
+}
+
+export default Index
